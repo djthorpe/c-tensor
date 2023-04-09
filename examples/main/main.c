04fa50f9ddac62cd14369fe72b46f44133891705
David Thorpe <djt@mutablelogic.com>
2023-04-09 19:43:26 +0200

    Adding graph evaluation

--- examples/main/main.c
+++ examples/main/main.c
@@ -13,9 +13,6 @@ int main()
     tensor_t *b = tensor_float32_vec(pool, (float[]){1, 2, 3}, 3);
     assert(b);
 
-    printf("a=%s\n", tensor_cstring(tensor_describe(pool, a)));
-    printf("b=%s\n", tensor_cstring(tensor_describe(pool, b)));
-
     // We cast a value to a float32
     tensor_t *c = tensor_cast_float32(pool, a);
     assert(c);
@@ -23,6 +20,9 @@ int main()
     // And then we multiply b by the scalar value c
     tensor_t *d = tensor_mul(pool, b, c);
     assert(d);
+
+    printf("computing graph....\n");
+
 
     // Now we need to create a graph with d as the output, in order
     // to evaluate it
--- src/tensor/graph.c
+++ src/tensor/graph.c
@@ -4,21 +4,67 @@
 #include "private.h"
 
 ///////////////////////////////////////////////////////////////////////////////
+// PRIVATE METHODS
+
+static void tensor_graph_visit(tensor_pool_t *pool, tensor_t *t)
+{
+    assert(pool != NULL);
+    assert(t != NULL);
+
+    // TODO: We don't visit the same node twice
+
+    tensor_debug(t->pool, "tensor_graph_visit: %s\n", tensor_cstring(tensor_describe(pool, t)));
+
+    // Visit the dependencies
+    if (t->a != NULL)
+    {
+        tensor_graph_visit(pool, t->a);
+    }
+    if (t->b != NULL)
+    {
+        tensor_graph_visit(pool, t->b);
+    }
+
+    // TODO: Mark the tensor as visited
+    // TODO: Push it onto the stack of evaluations
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // PUBLIC METHODS
 
 // Construct the graph which allows a tensor to be evaluated, returns NULL on error
-tensor_graph_t *tensor_graph_create(tensor_pool_t *pool, tensor_t *a) {
+tensor_graph_t *tensor_graph_create(tensor_pool_t *pool, tensor_t *t)
+{
     assert(pool != NULL);
-    assert(a != NULL);
+    assert(t != NULL);
+
+    // Create a graph
+    tensor_graph_t *graph = tensor_pool_alloc(pool, sizeof(tensor_graph_t));
+    if (graph == NULL)
+    {
+        return NULL;
+    }
+
+    // If a has no dependencies, then return t
+    if (t->op == NONE)
+    {
+        graph->root = t;
+        return graph;
+    }
+    assert(t->a != NULL || t->b != NULL);
+
+    // We go through all nodes in the graph
+    tensor_graph_visit(pool, t);
+
     // Not yet implemented
     return NULL;
 }
 
 // Perform the evaluation and return the output node (which has now been computed),
-// returns NULL on error 
-tensor_t* tensor_evaluate(tensor_graph_t* graph) {
+// returns NULL on error
+tensor_t *tensor_evaluate(tensor_graph_t *graph)
+{
     assert(graph != NULL);
     // Not yet implemented
     return NULL;
 }
-
