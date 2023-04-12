cd59ac67d42d4e814cbbc04ebe1251d4d66a9d6d
David Thorpe <djt@mutablelogic.com>
2023-04-12 08:55:05 +0200

    Updated tests

--- src/string/csv.c
+++ src/string/csv.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 
 #include <tensor/tensor.h>
@@ -8,34 +9,36 @@
 ///////////////////////////////////////////////////////////////////////////////
 // PRIVATE METHODS
 
-
 ///////////////////////////////////////////////////////////////////////////////
 // PUBLIC METHODS
 
 // Create a CSV parser, with the given separator (usually ',', ';' or '\t')
-tensor_str_csv_t *tensor_str_csv_create(tensor_pool_t *pool, const char sep) {
+tensor_str_csv_t *tensor_str_csv_create(tensor_pool_t *pool, const char sep)
+{
     assert(pool != NULL);
 
     tensor_str_csv_t *csv = tensor_pool_alloc(pool, sizeof(tensor_str_csv_t), NULL);
-    if (csv == NULL) {
+    if (csv == NULL)
+    {
         return NULL;
     }
 
     // Initialize the CSV parser
     csv->pool = pool;
-    csv->sep = sep;
+    csv->sep = sep ? sep : ',';
 
     // Allocate the delimiters we are going to use
     // to split the tokens
     csv->delimiters = tensor_pool_alloc(pool, 5, NULL);
-    if (csv->delimiters == NULL) {
+    if (csv->delimiters == NULL)
+    {
         return NULL;
     }
-    csv->delimiters[0] = sep;
+    csv->delimiters[0] = '#';
     csv->delimiters[1] = '"';
     csv->delimiters[2] = '\n';
-    csv->delimiters[3] = '#';
-    csv->delimiters[4] = '\0';
+    csv->delimiters[3] = csv->sep;
+    csv->delimiters[4] = 0;
 
     // Return success
     return csv;
@@ -43,20 +46,24 @@ tensor_str_csv_t *tensor_str_csv_create(tensor_pool_t *pool, const char sep) {
 
 // Consume a string of CSV data and return the fields as a list of tokens.
 // Returns NULL on error.
-tensor_str_token_t *tensor_str_csv_parseline(tensor_str_csv_t *csv, tensor_str_t *str, void *user_data) {
+tensor_str_token_t *tensor_str_csv_parseline(tensor_str_csv_t *csv, tensor_str_t *str, void *user_data)
+{
     assert(csv != NULL);
     assert(str != NULL);
 
     // Tokenize the string
-    tensor_str_token_t* token = tensor_str_tokenize(csv->pool,str,csv->delimiters,true,user_data);
-    if (token == NULL) {
+    tensor_str_token_t *head = tensor_str_tokenize(csv->pool, str, csv->delimiters, true, user_data);
+    if (head == NULL)
+    {
         return NULL;
     }
 
-    while(token) {
+    tensor_str_token_t *token = head;
+    while (token)
+    {
         // TODO
         token = tensor_str_token_next(token);
     }
 
-    return NULL;
+    return head;
 }
--- tests/string/csv.c
+++ tests/string/csv.c
@@ -4,32 +4,52 @@
 #include <tensor/string.h>
 #include "test.h"
 
+
+#define buf_size 80
+static char buf[buf_size];
+
 // Simple case with two tokens and no quotes, with no trailing newline
 // rows = 1 tokens = 2
-const char* test_100 = "Hello,World";
+const char *test_100 = "Hello,World";
 
 // More complex case with quotes and newlines
 // rows = 5 tokens = 5
-const char* test_101 = "Year,Make,Model,Description,Price\n1997,Ford,E350,\"ac, abs, moon\",3000.00\n1999,Chevy,\"Venture \"\"Extended Edition\"\"\",\"\",4900.00\n1999,Chevy,\"Venture ""Extended Edition, Very Large\"\"\",\"\",5000.00\n1996,Jeep,Grand Cherokee,\"MUST SELL!\nair, moon roof, loaded\",4799.00\n";
+const char *test_101 = "Year,Make,Model,Description,Price\n1997,Ford,E350,\"ac, abs, moon\",3000.00\n1999,Chevy,\"Venture \"\"Extended Edition\"\"\",\"\",4900.00\n1999,Chevy,\"Venture "
+                       "Extended Edition, Very Large\"\"\",\"\",5000.00\n1996,Jeep,Grand Cherokee,\"MUST SELL!\nair, moon roof, loaded\",4799.00\n";
 
 // Simple case with blank lines
 // rows = 3 tokens = 4
-const char* test_102 = "Year,Make,Model,Length\n\n1997,Ford,E350,2.35\n2000,Mercury,Cougar,2.38\n\n\n";
+const char *test_102 = "Year,Make,Model,Length\n\n1997,Ford,E350,2.35\n2000,Mercury,Cougar,2.38\n\n\n";
 
 // Case with semicolon as delimiter
 // rows = 3 tokens = 4
-const char* test_103 = "\n\nYear;Make;Model;Length\n1997;Ford;E350;2,35\n2000;Mercury;Cougar;2,38\n";
+const char *test_103 = "\n\nYear;Make;Model;Length\n1997;Ford;E350;2,35\n2000;Mercury;Cougar;2,38\n";
 
-void test_csv_000()
+void test_csv_100()
 {
-    printf("Running test_csv_000\n");
+    printf("Running test_csv_100\n");
     tensor_pool_t *pool = tensor_pool_create(1024);
     ASSERT_NOTNULL(pool);
-    
+
+    // Test the simple case
+    tensor_str_csv_t *csv = tensor_str_csv_create(pool, 0);
+    ASSERT_NOTNULL(csv);
+    tensor_str_t *str = tensor_str_create(pool, (char *)test_100);
+    ASSERT_NOTNULL(str);
+    tensor_str_token_t *token = tensor_str_csv_parseline(csv, str, NULL);
+    ASSERT_NOTNULL(token);
+
+    printf("  csv=%s\n", test_100);
+    while (token)
+    {
+        printf("  token=%s\n", tensor_cstring(buf, buf_size, tensor_str_token_describe(pool, token)));
+        token = tensor_str_token_next(token);
+    }
+
     tensor_pool_destroy(pool);
 }
 
 void test_csv()
 {
-    test_csv_000();
+    test_csv_100();
 }
