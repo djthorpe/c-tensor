d85c891a1c1b60c0bd0e788fab17d19f4878e30c
David Thorpe <djt@mutablelogic.com>
2023-04-12 13:06:05 +0200

    Added rand

--- include/tensor/math.h
+++ include/tensor/math.h
@@ -7,4 +7,15 @@
 // Opaque random number generator
 typedef struct tensor_math_rand_generator_instance tensor_math_rand_generator_t;
 
+
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+ * Generate a random number between 0.0 and 1.0 using a generator seed
+ *
+ * @param generator Random number generator
+ * @return Random number between 0.0 and 1.0
+*/
+double tensor_math_rand_float64(tensor_math_rand_generator_t *generator)
+
 #endif
--- src/math/rand.h
+++ src/math/rand.h
@@ -1,11 +1,11 @@
 #ifndef TENSOR_PRIVATE_MATH_RAND_H
 #define TENSOR_PRIVATE_MATH_RAND_H
 
-#include <types.h>
+#include <inttypes.h>
 #include "tensor_private.h"
 
-// String data structure
-struct tensor_math_generator_instance
+// Random number generator
+struct tensor_math_rand_generator_instance
 {
     int64_t seed;
 };
