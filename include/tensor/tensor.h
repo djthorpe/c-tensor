cdc01a8daba823041b78193a97ba469491d13f43
David Thorpe <djt@mutablelogic.com>
2023-04-12 11:43:58 +0200

    Added documentation

--- include/tensor/tensor.h
+++ include/tensor/tensor.h
@@ -74,41 +74,130 @@ extern tensor_t *tensor_float64_vec(tensor_pool_t *pool, double *values, uint32_
 ///////////////////////////////////////////////////////////////////////////////
 // OPERATIONS
 
-// Create an operation node to cast to a specific type
+/**
+ * Cast a tensor to a different data type
+ * 
+ * @param pool        The memory pool to use for creating the tensor operation
+ * @param type        The data type to cast to
+ * @param a           The tensor argument
+ * @return            The tensor which is the output node of the operation.
+ *                    Returns NULL on error. Typically the error will be due
+ *                    to insufficient memory in the pool. If the data type
+ *                    of the tensor is already the same as the requested type,
+ *                    then the tensor is returned unchanged.
+ */
 extern tensor_t *tensor_cast(tensor_pool_t *pool, tensor_dtype_t type, tensor_t *a);
 
-// Perform multiplication on two tensors, or one tensor and a scalar
+/**
+ * Create a multiplication tensor operation on two tensors
+ * 
+ * Multiplication can occur on two tensors of the same shape, or if
+ * one tensor is a scalar. In the latter case, the scalar is broadcast
+ * to the shape of the other tensor.
+ * 
+ * @param pool        The memory pool to use for creating the tensor operation
+ * @param a           The first tensor argument
+ * @param b           The second tensor argument
+ * @return            The tensor which is the output node of the operation,
+ *                    or NULL on error. Typically the error will be due to
+ *                    insufficient memory in the pool, or the data types of
+ *                    the tensors are not not same.
+ */
 extern tensor_t *tensor_mul(tensor_pool_t *pool, tensor_t *a, tensor_t *b);
 
 ///////////////////////////////////////////////////////////////////////////////
 // GRAPHS
 
-// Construct the graph which allows a to be evaluated, returns NULL on error
+/**
+ * Construct a graph which allows a tensor calculation to be evaluated
+ * 
+ * @param pool        The memory pool to use for creating the graph
+ * @param a           The tensor to evaluate
+ * @return            The tensor graph, or NULL on error.
+ *                    Typically the error will be due to insufficient memory
+ *                    in the pool or because the graph could not be
+ *                    constructed, due to circular dependencies.
+ */
 extern tensor_graph_t *tensor_graph_create(tensor_pool_t *pool, tensor_t *a);
 
-// Perform the evaluation and return the output node, returns NULL on error
+/**
+ * Perform the evaluation and return the output node
+ * 
+ * @param graph       The graph to evaluate
+ * @return            The tensor which is the output node of the graph, or NULL
+ *                    if the graph could not be evaluated, usually due to a
+ *                    failing tensor operation.
+ */
 extern tensor_t *tensor_graph_evaluate(tensor_graph_t *graph);
 
 ///////////////////////////////////////////////////////////////////////////////
 // POOL
 
-// Create a new tensor pool, returns NULL on error
+/**
+ * Create a new memory pool
+ * 
+ * The memory pool is used to a fixed allocation of memory for tensors and graphs.
+ * Ensure you allocate enough memory for the pool. The pool is also used to allocate
+ * memory for strings and tokens, but as strings can be of arbitrary length, the
+ * string data is not stored in the pool.
+ *
+ * @param memsize     The size of the memory pool in bytes
+ * @return            The memory pool, or NULL if it could not be created due to
+ *                    insufficient memory.
+ */
 extern tensor_pool_t *tensor_pool_create(uint32_t memsize);
 
-// Destroy tensor pool
+/**
+ * Destroy a memory pool, freeing all resources
+ * 
+ * The memory resources (including string data) is freed, and the memory is 
+ * returned to the system.
+ *
+ * @param pool        The memory pool
+ */
 extern void tensor_pool_destroy(tensor_pool_t *pool);
 
-// Allocate N bytes from the pool, returns NULL on error
-// and set a unique id for the allocation if id is not NULL
+/**
+ * Allocate memory from the memory pool and set a unique id for the memory
+ * allocation.
+ * 
+ * @param pool        The memory pool
+ * @param size        The size of the memory allocation in bytes
+ * @param id          A pointer reference to a unique identifier to be set 
+ *                    for the memory allocation. Pass NULL if this is not
+ *                    required.
+ * @return            A pointer to the memory allocation, or NULL if the
+ *                    allocation failed due to insufficient memory. In this
+ *                    case, increase the size of the memory pool when creating
+ *                    the pool.
+ */
 extern void *tensor_pool_alloc(tensor_pool_t *pool, size_t size, uint32_t *id);
 
-// Return size of memory pool
+/**
+ * Return the total size of the memory pool in bytes
+ * 
+ * @param pool        The memory pool
+ * @return            The size of the memory pool in bytes, not including
+ *                    string data.
+ */
 extern size_t tensor_pool_size(tensor_pool_t *pool);
 
-// Return used bytes of memory pool
+/**
+ * Return the used size of the memory pool in bytes
+ * 
+ * @param pool        The memory pool
+ * @return            The size of the used memory pool in bytes, not including
+ *                    string data.
+ */
 extern size_t tensor_pool_used(tensor_pool_t *pool);
 
-// Return used bytes of string data in the pool (total memory used is pool_used + pool_str_used)
+/**
+ * Return the used size for string data in bytes
+ * 
+ * @param pool        The memory pool
+ * @return            The size of the string data in bytes, not including
+ *                    the memory pool size.
+ */
 extern size_t tensor_pool_str_used(tensor_pool_t *pool);
 
 #endif
