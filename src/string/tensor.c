5473a34fe6eca54caad94f3c00c63a9ba8e406e5
David Thorpe <djt@mutablelogic.com>
2023-04-14 15:00:00 +0200

    Added scalar tests

--- src/string/tensor.c
+++ src/string/tensor.c
@@ -9,12 +9,33 @@
 ///////////////////////////////////////////////////////////////////////////////
 // PRIVATE METHODS
 
-///////////////////////////////////////////////////////////////////////////////
-// PUBLIC METHODS
+static bool tensor_str_print_dtype(tensor_str_t *str, tensor_dtype_t type, void *data)
+{
+    assert(str);
+    assert(data);
+
+    switch (type)
+    {
+    case UINT32_T:
+        return tensor_str_printf(str, "%u", *(uint32_t *)data);
+    case INT32_T:
+        return tensor_str_printf(str, "%d", *(int32_t *)data);
+    case UINT64_T:
+        return tensor_str_printf(str, "%lu", *(uint64_t *)data);
+    case INT64_T:
+        return tensor_str_printf(str, "%ld", *(int64_t *)data);
+    case FLOAT32_T:
+        return tensor_str_printf(str, "%.4f", *(float *)data);
+    case FLOAT64_T:
+        return tensor_str_printf(str, "%.4lf", *(double *)data);
+    default:
+        return false;
+    }
+}
 
 static const char *tensor_type_str(tensor_t *tensor)
 {
-    assert(tensor != NULL);
+    assert(tensor);
 
     if (tensor->op == NONE)
     {
@@ -33,6 +54,9 @@ static const char *tensor_type_str(tensor_t *tensor)
     }
     return tensor_op_str(tensor->op);
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// PUBLIC METHODS
 
 const char *tensor_dtype_str(tensor_dtype_t dtype)
 {
@@ -102,5 +126,85 @@ tensor_str_t *tensor_str_describe(tensor_pool_t *pool, tensor_t *tensor)
  */
 tensor_str_t *tensor_str_print(tensor_pool_t *pool, tensor_t *tensor)
 {
-    return NULL;
+    assert(pool);
+    assert(tensor);
+    assert(tensor->data);
+
+    tensor_str_t *str = tensor_str_create(pool, NULL);
+    if (str == NULL)
+    {
+        return NULL;
+    }
+
+    // If the tensor is a scalar, print it
+    if (tensor_is_scalar(tensor))
+    {
+        return tensor_str_print_dtype(str, tensor->dtype, tensor->data) ? str : NULL;
+    }
+
+    void *data = tensor->data;
+    uint32_t index[tensor->ndims];
+    memset(index, 0, sizeof(uint32_t) * tensor->ndims);
+    for (uint8_t i = 0; i < tensor->nvalues; i++)
+    {
+        // Print a '[' for each index which is zero
+        for (uint8_t j = 0; j < tensor->ndims; j++)
+        {
+            if (index[j] == 0)
+            {
+                if (!tensor_str_printf(str, "["))
+                {
+                    return NULL;
+                }
+            }
+        }
+
+        // Print the element
+        if (!tensor_str_print_dtype(str, tensor->dtype, data))
+        {
+            return NULL;
+        }
+
+        // Print a ']' for each index which is maximum, else print a ,
+        bool printed_comma = true;
+        for (uint8_t j = 0; j < tensor->ndims; j++)
+        {
+            if (index[j] == tensor->dims[j] - 1)
+            {
+                printed_comma = false;
+                if (!tensor_str_printf(str, "]"))
+                {
+                    return NULL;
+                }
+            }
+        }
+
+        // Append a value separator
+        if (printed_comma)
+        {
+            if (!tensor_str_printf(str, ","))
+            {
+                return NULL;
+            }
+        }
+
+        // Increment the index
+        for (uint8_t j = 0; j < tensor->ndims; j++)
+        {
+            index[j]++;
+            if (index[j] < tensor->dims[j])
+            {
+                break;
+            }
+            else
+            {
+                index[j] = 0;
+            }
+        }
+
+        // Next element
+        data += tensor_dtype_sizeof(tensor->dtype);
+    }
+
+    return str;
 }
--- tests/string/tensor.c
+++ tests/string/tensor.c
@@ -133,9 +133,201 @@ void test_tensor_010()
     tensor_pool_destroy(pool);
 }
 
+void test_tensor_020()
+{
+    printf("Running test_tensor_020 (vector int32 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *vector = tensor_int32_vec(pool, 4, (int32_t[]){12, -34, 56, -78});
+    ASSERT_NOTNULL(vector);
+
+    tensor_str_t *str = tensor_str_print(pool, vector);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("[12,-34,56,-78]", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_021()
+{
+    printf("Running test_tensor_021 (vector uint32 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *vector = tensor_uint32_vec(pool, 4, (uint32_t[]){12, 34, 56, 78});
+    ASSERT_NOTNULL(vector);
+
+    tensor_str_t *str = tensor_str_print(pool, vector);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("[12,34,56,78]", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_022()
+{
+    printf("Running test_tensor_022 (vector int64 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *vector = tensor_int64_vec(pool, 4, (int64_t[]){12, -34, 56, -78});
+    ASSERT_NOTNULL(vector);
+
+    tensor_str_t *str = tensor_str_print(pool, vector);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("[12,-34,56,-78]", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_023()
+{
+    printf("Running test_tensor_023 (vector uint64 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *vector = tensor_uint64_vec(pool, 4, (uint64_t[]){12, 34, 56, 78});
+    ASSERT_NOTNULL(vector);
+
+    tensor_str_t *str = tensor_str_print(pool, vector);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("[12,34,56,78]", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_024()
+{
+    printf("Running test_tensor_024 (vector float32 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *vector = tensor_float32_vec(pool, 4, (float[]){1.2, 3.4, 5.6, 7.8});
+    ASSERT_NOTNULL(vector);
+
+    tensor_str_t *str = tensor_str_print(pool, vector);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("[1.2000,3.4000,5.6000,7.8000]", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_025()
+{
+    printf("Running test_tensor_025 (vector float64 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *vector = tensor_float64_vec(pool, 4, (double[]){1.2, 3.4, 5.6, 7.8});
+    ASSERT_NOTNULL(vector);
+
+    tensor_str_t *str = tensor_str_print(pool, vector);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("[1.2000,3.4000,5.6000,7.8000]", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_030()
+{
+    printf("Running test_tensor_030 (scalar int32 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *scalar= tensor_int32(pool, -78);
+    ASSERT_NOTNULL(scalar);
+
+    tensor_str_t *str = tensor_str_print(pool, scalar);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("-78", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_031()
+{
+    printf("Running test_tensor_031 (scalar uint32 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *scalar= tensor_uint32(pool, 78);
+    ASSERT_NOTNULL(scalar);
+
+    tensor_str_t *str = tensor_str_print(pool, scalar);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("78", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_032()
+{
+    printf("Running test_tensor_032 (scalar int64 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *scalar= tensor_int64(pool, -78910);
+    ASSERT_NOTNULL(scalar);
+
+    tensor_str_t *str = tensor_str_print(pool, scalar);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("-78910", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_033()
+{
+    printf("Running test_tensor_033 (scalar uint64 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *scalar= tensor_uint64(pool, 123456789);
+    ASSERT_NOTNULL(scalar);
+
+    tensor_str_t *str = tensor_str_print(pool, scalar);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("123456789", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_034()
+{
+    printf("Running test_tensor_034 (scalar float32 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *scalar= tensor_float32(pool, -3.1415);
+    ASSERT_NOTNULL(scalar);
+
+    tensor_str_t *str = tensor_str_print(pool, scalar);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("-3.1415", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
+void test_tensor_035()
+{
+    printf("Running test_tensor_035 (scalar float64 print)\n");
+    tensor_pool_t *pool = tensor_pool_create(1024);
+    ASSERT_NOTNULL(pool);
+
+    tensor_t *scalar= tensor_float64(pool, 3.1415);
+    ASSERT_NOTNULL(scalar);
+
+    tensor_str_t *str = tensor_str_print(pool, scalar);
+    ASSERT_NOTNULL(str);
+    ASSERT_EQUALS_STR("3.1415", tensor_cstring(buf, buf_size, str));
+
+    tensor_pool_destroy(pool);
+}
+
 void test_tensor()
 {
-    // Scalars
+    // Describe Scalars
     test_tensor_000();
     test_tensor_001();
     test_tensor_002();
@@ -143,6 +335,22 @@ void test_tensor()
     test_tensor_004();
     test_tensor_005();
 
-    // Vectors
+    // Describe Vectors
     test_tensor_010();
+
+    // Print Vectors
+    test_tensor_020();
+    test_tensor_021();
+    test_tensor_022();
+    test_tensor_023();
+    test_tensor_024();
+    test_tensor_025();
+
+    // Print Scalars
+    test_tensor_030();
+    test_tensor_031();
+    test_tensor_032();
+    test_tensor_033();
+    test_tensor_034();
+    test_tensor_035();
 }
